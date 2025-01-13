9ddd4e46779174555043894db76dc4fbf1d71c3f
friedSaewoo <zriag@naver.com>
2025-01-13 13:49:53 +0900

    feat : Kiosk lv1 기능구현

--- src/main/java/com/example/lv1/Main.java
+++ src/main/java/com/example/lv1/Main.java
@@ -14,34 +14,30 @@ public class Main {
         menu.add("Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
         menu.add("Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
 
-        System.out.println(menu.get(0));
         while(true){
+            int a = 0;
             System.out.println("[ SHAKESHACK MENU ]");
             for (int i = 0 ;i< menu.size();i++){
                 System.out.println(i+1+". "+ menu.get(i));
             }
             System.out.println("0. 종료       | 종료");
-
-            int a = Integer.parseInt(sc.nextLine());
-
-            switch (a){
-                case 1:
-                    System.out.println(menu.get(a-1));
-                    continue;
-                case 2:
-                    System.out.println(menu.get(a-1));
-                    continue;
-                case 3:
-                    System.out.println(menu.get(a-1));
-                    continue;
-                case 4:
-                    System.out.println(menu.get(a-1));
-                    continue;
-                case 0:
-                    System.out.println("키오스크 종료");
-                    break;
+            try {
+                System.out.print("\n무엇을 하시겠습니까 ? : ");
+                a = Integer.parseInt(sc.nextLine());
+            }catch (Exception e){
+                System.out.println("올바른 선택지를 골라주세요!!");
+                continue;
             }
-            break;
+            if(a == 0){
+                System.out.println("키오스크를 종료합니다.");
+                break;
+            }else if(a <=menu.size()) {
+                System.out.println("**********************************************************************");
+                System.out.println("선택하신 메뉴 : "+menu.get(a-1));
+                System.out.println("**********************************************************************");
+            }else{
+                System.out.println("올바른 선택지를 골라주세요!!");
+            }
         }
     }
 }
