5231e46e88158ac7c9a3ab200e5755eaac8a24d3
friedSaewoo <zriag@naver.com>
2025-01-14 15:16:41 +0900

    feat : kiosk lv4_lv5 기능구현

---
+++ src/main/java/com/example/lv4_5/Kiosk.java
@@ -0,0 +1,90 @@
+package com.example.lv4_5;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+public class Kiosk {
+    private final List<Menu> menu;
+
+    public Kiosk() {
+        this.menu = new ArrayList<>();
+    }
+    // 카테고리를 출력해주는 첫 메뉴
+    private void showCategory() {
+        System.out.println("[ SHAKESHACK MENU ]");
+        for (int i = 0; i < menu.size(); i++) {
+            System.out.print(i + 1 + ". ");
+            System.out.printf("%-15s", menu.get(i).getCategory());
+            System.out.println();
+        }
+        System.out.println("0. 종료");
+    }
+
+    // 키오스크 실행
+    public void start() {
+        while (true) {
+            showCategory(); // 카테고리 출력
+            int categoryOption = inputInteger();
+
+            if (categoryOption == 0) {
+                System.out.println("키오스크를 종료합니다.");
+                break;
+            }else if(categoryOption > menu.size()){
+                System.out.println();
+                System.out.println("\n올바른 선택지를 입력하세요!!\n");
+            }else{
+                while (true) {
+                    menu.get(categoryOption - 1).showMenuList();
+                    int menuOption = inputInteger();
+                    if (menuOption == 0) {
+                        break;
+                    } else if (menuOption > menu.get(categoryOption-1).getMenuList().size()) {
+                        System.out.println("\n올바른 선택지를 입력하세요!!\n");
+                    } else{
+                        menu.get(categoryOption-1).showMenuInfo(menuOption);
+                      break;
+                    }
+                }
+            }
+        }
+    }
+
+    // category, menuItem 추가 메서드
+    public void addMenu(String categoryName, MenuItem menuItem) {
+        // 입력한 카테고리가 이미 존재하는지 확인
+        Optional<Menu> menuOptional = menu.stream()
+                .filter(menu -> menu.getCategory().equals(categoryName))
+                .findFirst();
+        // 카테고리가 이미 존재한다면, 해당 카테고리에 menuItem 추가
+        if (menuOptional.isPresent()) {
+            menuOptional.get().addMenuItem(menuItem);
+        } else {
+            // 카테고리가 존재하지 않으면 카테고리(Menu) 객체를 새로 생성하고 해당 카테고리에 menuItem추가
+            Menu newMenu = new Menu(categoryName);
+            newMenu.addMenuItem(menuItem);
+            menu.add(newMenu);
+        }
+    }
+    
+    // 정수를 입력받는 메서드
+    private int inputInteger() {
+        Scanner sc = new Scanner(System.in);
+        int input;
+        while (true) {
+            try {
+                System.out.print("입력 : ");
+                input = Integer.parseInt(sc.nextLine());
+            } catch (Exception e) {
+                System.out.println("\n올바른 선택지를 입력하세요!!\n");
+                continue;
+            }
+            if (input >= 0) {
+                return input;
+            } else {
+                System.out.println("\n올바른 선택지를 입력하세요!!\n");
+            }
+        }
+    }
+}
---
+++ src/main/java/com/example/lv4_5/Menu.java
@@ -0,0 +1,55 @@
+package com.example.lv4_5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu {
+    private final String category;
+    List<MenuItem> menuList;
+    
+    // 카테고리를 매개변수로 받고, List는 초기화만 해주는 생성자, List는 메서드로 추가
+    public Menu(String category) {
+        this.category = category;
+        this.menuList = new ArrayList<>();
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public List<MenuItem> getMenuList() {
+        return menuList;
+    }
+    // menuItem을 추가해주는 메서드
+    public void addMenuItem(MenuItem menuItem){
+        this.menuList.add(menuItem);
+    }
+
+    // 카테고리별 메뉴 리스트 출력
+    public void showMenuList() {
+        System.out.println("[ SHAKESHACK MENU ]");
+        for (int i = 0; i < menuList.size(); i++) {
+            System.out.print(i + 1 + ". ");
+            System.out.printf("%-15s", menuList.get(i).getMenuName());
+            System.out.printf("| w %-5s| ", menuList.get(i).getMenuPrice());
+            System.out.println(menuList.get(i).getMenuInfo());
+        }
+        System.out.println("0. 뒤로가기");
+    }
+
+    // 선택한 메뉴 정보를 출력하는 메서드
+    public void showMenuInfo (int menu){
+        try {
+            String name = menuList.get(menu-1).getMenuName();
+            String age = menuList.get(menu - 1).getMenuPrice();
+            String info = menuList.get(menu - 1).getMenuInfo();
+            System.out.println("\n***************************************");
+            System.out.println("메뉴 명 :" + name);
+            System.out.println("가격 :" + age);
+            System.out.println("정보 :" + info);
+            System.out.println("***************************************\n");
+        } catch (IndexOutOfBoundsException ioe) {
+            System.out.println("\n올바른 선택지를 입력하세요!!\n");
+        }
+    }
+}
