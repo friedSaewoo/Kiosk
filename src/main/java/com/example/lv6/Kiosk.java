755bbc376902125b4c415046b4db39bd052defaf
friedSaewoo <zriag@naver.com>
2025-01-16 14:57:20 +0900

    fix : ìkiosk 클래스 ì±리팩토링책ì((책임분산)

--- src/main/java/com/example/lv6/Kiosk.java
+++ src/main/java/com/example/lv6/Kiosk.java
@@ -3,97 +3,124 @@ package com.example.lv6;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Scanner;
 
 import static com.example.lv6.InputInteger.inputInteger;
 
 public class Kiosk {
     private final List<Menu> menu;
     private final Cart cart = new Cart();
-    private final Scanner sc = new Scanner(System.in);
+    private boolean cartInMenu = false;
 
     public Kiosk() {
         this.menu = new ArrayList<>();
+        constructMenu();
     }
 
-
+    private void constructMenu() {
+        // 메뉴추가
+        // 버거
+        addMenu("Burgers", new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        addMenu("Burgers", new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        addMenu("Burgers", new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        addMenu("Burgers", new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+        // 드링크
+        addMenu("Drinks", new MenuItem("Coke", 2.5, "콜라, 탄산음료"));
+        addMenu("Drinks", new MenuItem("Sprite", 2.5, "청량한 레몬향의 탄산음료"));
+        addMenu("Drinks", new MenuItem("Lemonade", 3.0, "상큼한 레몬 주스"));
+        addMenu("Drinks", new MenuItem("Iced Tea", 3.2, "시원한 아이스 티"));
+        // 디저트
+        addMenu("Desserts", new MenuItem("Milkshake", 5.5, "바닐라 아이스크림과 우유로 만든 밀크쉐이크"));
+        addMenu("Desserts", new MenuItem("Chocolate Cake", 4.0, "진한 초콜릿 케이크"));
+        addMenu("Desserts", new MenuItem("Cheesecake", 4.5, "부드럽고 고소한 치즈케이크"));
+        addMenu("Desserts", new MenuItem("Apple Pie", 3.5, "따뜻한 애플 파이"));
+    }
 
     // 키오스크 실행
     public void start() {
-        boolean cartInMenu = false; // 장바구니에 물건이 있는지 여부 확인
+        System.out.println("키오스크 시작");
         while (true) {
-            showCategory(); // 카테고리 출력
-            if (cartInMenu) {
-                System.out.println("\n[ Order Menu ]");
-                System.out.println("4. Orders");
-                System.out.println("5. Cancel");
-            }
-            System.out.println("0. 종료");
+            showCategory(cartInMenu); // 카테고리 출력
             // 카테고리 선택
             int categoryOption = inputInteger();
-            
-            // 올바른 카테고리를 입력할때까지 반복
             if (categoryOption == 0) {
                 System.out.println("키오스크를 종료합니다.");
                 break;
-            }else if((categoryOption == 4 || categoryOption == 5) && cartInMenu ){
+            }
+            handleCategorySelection(categoryOption);
+        }
+    }
 
-                    if(categoryOption == 4){
-                        cart.showCart();
-                        cartInMenu = !cart.cartIsEmpty();
-                    }else{
-                        cart.cleanCart();
-                        cartInMenu=false;
-                    }
+    private void handleCategorySelection(int categoryOption){
+        if ((categoryOption == 4 || categoryOption == 5) && cartInMenu) {
 
-            } else if (categoryOption > menu.size()) {
-                System.out.println();
+            if (categoryOption == 4) {
+                cart.showCart();
+                cartInMenu = !cart.cartIsEmpty();
+            } else {
+                cart.cleanCart();
+                cartInMenu = false;
+            }
+
+        } else if (categoryOption > menu.size()) {
+            System.out.println("\n올바른 선택지를 입력하세요!!\n");
+        } else {
+            handleMenuSelection(categoryOption);
+        }
+    }
+
+    private void handleMenuSelection(int categoryOption) {
+        // 올바른 메뉴를 입력할때까지 반복
+        while (true) {
+            // 메뉴 선택
+            menu.get(categoryOption - 1).showMenuList();
+            int menuOption = inputInteger();
+            if (menuOption == 0) {
+                break;
+            } else if (menuOption > menu.get(categoryOption - 1).getMenuList().size()) { // 메뉴 리스트 사이즈로 범위 제한
                 System.out.println("\n올바른 선택지를 입력하세요!!\n");
             } else {
-                // 올바른 메뉴를 입력할때까지 반복
-                while (true) {
-                    // 메뉴 선택
-                    menu.get(categoryOption - 1).showMenuList();
-                    int menuOption = inputInteger();
-                    if (menuOption == 0) {
-                        break;
-                    } else if (menuOption > menu.get(categoryOption - 1).getMenuList().size()) { // 메뉴 리스트 사이즈로 범위 제한
-                        System.out.println("\n올바른 선택지를 입력하세요!!\n");
-                    } else {
-                        menu.get(categoryOption - 1).showMenuInfo(menuOption);
-                        System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
-                        System.out.println("1. 확인            2. 취소");
+                buyMenu(categoryOption, menuOption);
+                break;
+            }
+        }
+    }
 
-                        // 선택한 메뉴 정보 확인 후 구매/취소 선택
-                        while (true) {
-                            int buyOption = inputInteger();
-                            if(buyOption == 1){
-                                cart.addMenuToCart(menu.get(categoryOption-1).getMenuList().get(menuOption-1));
-                                if (!cartInMenu){
-                                    cartInMenu=true;
-                                }
-                                break;
-                            }else if (buyOption >=3){
-                                System.out.println("\n올바른 선택지를 입력하세요!!\n");
-                            }else{
-                                break;
-                            }
-                        }
-                        break;
-                    }
+    private void buyMenu(int categoryOption, int menuOption) {
+        menu.get(categoryOption - 1).showMenuInfo(menuOption);
+        System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
+        System.out.println("1. 확인            2. 취소");
+
+        // 선택한 메뉴 정보 확인 후 구매/취소 선택
+        while (true) {
+            int buyOption = inputInteger();
+            if (buyOption == 1) {
+                cart.addMenuToCart(menu.get(categoryOption - 1).getMenuList().get(menuOption - 1));
+                if (!cartInMenu) {
+                    cartInMenu = true;
                 }
+                break;
+            } else if (buyOption >= 3) {
+                System.out.println("\n올바른 선택지를 입력하세요!!\n");
+            } else {
+                break;
             }
         }
     }
 
     // 카테고리를 출력해주는 첫 메뉴
-    private void showCategory() {
+    private void showCategory(boolean cartInMenu) {
         System.out.println("\n[ MAIN MENU ]");
         for (int i = 0; i < menu.size(); i++) {
             System.out.print(i + 1 + ". ");
             System.out.printf("%-15s", menu.get(i).getCategory());
             System.out.println();
         }
+        if (cartInMenu) {
+            System.out.println("\n[ Order Menu ]");
+            System.out.println("4. Orders");
+            System.out.println("5. Cancel");
+        }
+        System.out.println("0. 종료");
     }
 
     // category, menuItem 추가 메서드
@@ -112,7 +139,4 @@ public class Kiosk {
             menu.add(newMenu);
         }
     }
-
-    // 정수를 입력받는 메서드
-
 }
--- src/main/java/com/example/lv6/Main.java
+++ src/main/java/com/example/lv6/Main.java
@@ -4,24 +4,6 @@ package com.example.lv6;
 public class Main {
     public static void main(String[] args) {
         Kiosk kiosk = new Kiosk();
-
-        // 메뉴추가
-        // 버거
-        kiosk.addMenu("Burgers", new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        kiosk.addMenu("Burgers", new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-        kiosk.addMenu("Burgers", new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        kiosk.addMenu("Burgers", new MenuItem("Hamburger", 5.4 , "비프패티를 기반으로 야채가 들어간 기본버거"));
-        // 드링크
-        kiosk.addMenu("Drinks", new MenuItem("Coke", 2.5, "콜라, 탄산음료"));
-        kiosk.addMenu("Drinks", new MenuItem("Sprite", 2.5, "청량한 레몬향의 탄산음료"));
-        kiosk.addMenu("Drinks", new MenuItem("Lemonade", 3.0, "상큼한 레몬 주스"));
-        kiosk.addMenu("Drinks", new MenuItem("Iced Tea", 3.2, "시원한 아이스 티"));
-        // 디저트
-        kiosk.addMenu("Desserts", new MenuItem("Milkshake", 5.5, "바닐라 아이스크림과 우유로 만든 밀크쉐이크"));
-        kiosk.addMenu("Desserts", new MenuItem("Chocolate Cake", 4.0, "진한 초콜릿 케이크"));
-        kiosk.addMenu("Desserts", new MenuItem("Cheesecake", 4.5, "부드럽고 고소한 치즈케이크"));
-        kiosk.addMenu("Desserts", new MenuItem("Apple Pie", 3.5, "따뜻한 애플 파이"));
-
         kiosk.start();
     }
 }
